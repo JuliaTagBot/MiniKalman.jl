fff8c0f368f2bd9b6366d31ec81272496bed8623
cstjean <cedric.stjean@gmail.com>
2018-05-24 13:47:16 -0400

    Renaming. Remove some Kalmans

--- src/kalman_models.jl
+++ src/kalman_models.jl
@@ -4,8 +4,8 @@ include("identities.jl")
 
 export @kalman_model, Positive
 
-abstract type KalmanModel end
-abstract type KalmanInputs end
+abstract type Model end
+abstract type Inputs end
 
 struct Unspecified end
 
@@ -45,9 +45,9 @@ macro kalman_model(def)
     unspecified_kw(k) = Expr(:kw, k, :($MiniKalman.Unspecified()))
 
     esc(quote
-        $MiniKalman.@qstruct $model_type(; $(map(unspecified_kw, param_vars)...)) <: $MiniKalman.KalmanModel
+        $MiniKalman.@qstruct $model_type(; $(map(unspecified_kw, param_vars)...)) <: $MiniKalman.Model
         $MiniKalman.@qstruct $inputs_type(model::$model_type;
-                                          $(map(unspecified_kw, input_vars)...)) <: $MiniKalman.KalmanInputs
+                                          $(map(unspecified_kw, input_vars)...)) <: $MiniKalman.Inputs
         $(fundefs...)
         $MiniKalman.input_type(::Type{<:$model_type}) = $inputs_type
         $model_type
@@ -55,48 +55,48 @@ macro kalman_model(def)
 end
 
 # This definition assumes that the observations are specified, which is obviously
-# wrong when we're sampling. Maybe we should store `N` inside KalmanInputs, and specify
+# wrong when we're sampling. Maybe we should store `N` inside Inputs, and specify
 # it (optionally?) in `mk_inputs`?
 # Alternatively, we could force the user to specify `observation_mats` or
 # `observation_noises`. But I think I like `N` better.
-Base.length(inputs::KalmanInputs) = length(observations(inputs))
+Base.length(inputs::Inputs) = length(observations(inputs))
 
 ################################################################################
 ## Defaults
-transition_mat(inputs::KalmanInputs) = Identity()
-transition_mats(inputs::KalmanInputs) = Fill(transition_mat(inputs), length(inputs))
-transition_noise(inputs::KalmanInputs) = no_noise()
-transition_noises(inputs::KalmanInputs) = Fill(transition_noise(inputs), length(inputs))
-# observation_noise(inputs::KalmanInputs) = no_noise() is tempting, but it's
+transition_mat(inputs::Inputs) = Identity()
+transition_mats(inputs::Inputs) = Fill(transition_mat(inputs), length(inputs))
+transition_noise(inputs::Inputs) = no_noise()
+transition_noises(inputs::Inputs) = Fill(transition_noise(inputs), length(inputs))
+# observation_noise(inputs::Inputs) = no_noise() is tempting, but it's
 # a degenerate Kalman model, which causes problems
-observation_noises(inputs::KalmanInputs) = Fill(observation_noise(inputs), length(inputs))
-observation_mat(inputs::KalmanInputs) = Identity()
-observation_mats(inputs::KalmanInputs) = Fill(observation_mat(inputs), length(inputs))
+observation_noises(inputs::Inputs) = Fill(observation_noise(inputs), length(inputs))
+observation_mat(inputs::Inputs) = Identity()
+observation_mats(inputs::Inputs) = Fill(observation_mat(inputs), length(inputs))
 
 
 ################################################################################
 ## Delegations
 
-# Rename to `KalmanInputs(model; kwargs...)`?
-mk_inputs(model::KalmanModel; kwargs...) = input_type(typeof(model))(model; kwargs...)
+# Rename to `Inputs(model; kwargs...)`?
+mk_inputs(model::Model; kwargs...) = input_type(typeof(model))(model; kwargs...)
 
-kalman_filter(model::KalmanModel, initial_state; kwargs...) =
+kalman_filter(model::Model, initial_state; kwargs...) =
     kalman_filter(mk_inputs(model; kwargs...), initial_state)
-kalman_filter(inputs::KalmanInputs, initial_state) = 
+kalman_filter(inputs::Inputs, initial_state) = 
     kalman_filter(initial_state, observations(inputs),
                   observation_noises(inputs);
                   transition_mats=transition_mats(inputs),
                   transition_noises=transition_noises(inputs),
                   observation_mats=observation_mats(inputs))
 
-kalman_smoother(inputs::KalmanInputs, filtered_states::AbstractArray{<:Gaussian}) = 
+kalman_smoother(inputs::Inputs, filtered_states::AbstractArray{<:Gaussian}) = 
     kalman_smoother(filtered_states;
                     transition_mats=transition_mats(inputs),
                     transition_noises=transition_noises(inputs))
-kalman_smoother(inputs::KalmanInputs, initial_state::Gaussian) =
+kalman_smoother(inputs::Inputs, initial_state::Gaussian) =
     kalman_smoother(inputs, kalman_filter(inputs, initial_state)[1])
 
-kalman_sample(inputs::KalmanInputs, rng::AbstractRNG, initial_state) =
+kalman_sample(inputs::Inputs, rng::AbstractRNG, initial_state) =
     kalman_sample(rng, initial_state, observation_noises(inputs);
                   transition_mats=transition_mats(inputs),
                   transition_noises=transition_noises(inputs),
