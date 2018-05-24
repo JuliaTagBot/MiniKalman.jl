312813943baac7a1c60312ada6ea46de345fe572
cstjean <cedric.stjean@gmail.com>
2018-05-24 14:14:03 -0400

    Split model and inputs

--- src/kalman_models.jl
+++ src/kalman_models.jl
@@ -10,8 +10,7 @@ abstract type Inputs end
 struct Unspecified end
 
 
-kalman_quantities = [:observations,
-                     :observation_mat, :observation_mats, #:initial_state,
+kalman_quantities = [:observation_mat, :observation_mats, #:initial_state,
                      :observation_noise, :observation_noises, 
                      :transition_mat, :transition_mats,
                      :transition_noise, :transition_noises]
@@ -28,15 +27,15 @@ macro kalman_model(def)
     inputs_type = Symbol(model_type, "Inputs")
     param_vars = map(first ∘ splitarg, params)
 
-    @gensym km
+    @gensym km ki
     fundefs = map(qtydefs) do c
         @assert(@capture(c, fname_ := expr_), "Bad quantity definition: $c")
         @assert(fname in kalman_quantities,
                 "`$fname` is not a valid Kalman model quantity ($kalman_quantities)")
         quote
-            function $MiniKalman.$fname($km::$inputs_type)
-                $([:($p = $km.model.$p) for p in param_vars]...)
-                $([:($i = $km.$i) for i in input_vars]...)
+            function $MiniKalman.$fname($km::$model_type, $ki::$inputs_type)
+                $([:($p = $km.$p) for p in param_vars]...)
+                $([:($i = $ki.$i) for i in input_vars]...)
                 $expr
             end
         end
@@ -46,7 +45,7 @@ macro kalman_model(def)
 
     esc(quote
         $MiniKalman.@qstruct $model_type(; $(map(unspecified_kw, param_vars)...)) <: $MiniKalman.Model
-        $MiniKalman.@qstruct $inputs_type(model::$model_type;
+        $MiniKalman.@qstruct $inputs_type(_N::Int;
                                           $(map(unspecified_kw, input_vars)...)) <: $MiniKalman.Inputs
         $(fundefs...)
         $MiniKalman.input_type(::Type{<:$model_type}) = $inputs_type
@@ -54,53 +53,47 @@ macro kalman_model(def)
     end)
 end
 
-# This definition assumes that the observations are specified, which is obviously
-# wrong when we're sampling. Maybe we should store `N` inside Inputs, and specify
-# it (optionally?) in `mk_inputs`?
-# Alternatively, we could force the user to specify `observation_mats` or
-# `observation_noises`. But I think I like `N` better.
-Base.length(inputs::Inputs) = length(observations(inputs))
+Base.length(inputs::Inputs) = inputs._N
 
 ################################################################################
 ## Defaults
-transition_mat(inputs::Inputs) = Identity()
-transition_mats(inputs::Inputs) = Fill(transition_mat(inputs), length(inputs))
-transition_noise(inputs::Inputs) = no_noise()
-transition_noises(inputs::Inputs) = Fill(transition_noise(inputs), length(inputs))
+transition_mat(m, inputs::Inputs) = Identity()
+transition_mats(m, inputs::Inputs) = Fill(transition_mat(m, inputs), length(inputs))
+transition_noise(m, inputs::Inputs) = no_noise()
+transition_noises(m, inputs::Inputs) = Fill(transition_noise(m, inputs), length(inputs))
 # observation_noise(inputs::Inputs) = no_noise() is tempting, but it's
 # a degenerate Kalman model, which causes problems
-observation_noises(inputs::Inputs) = Fill(observation_noise(inputs), length(inputs))
-observation_mat(inputs::Inputs) = Identity()
-observation_mats(inputs::Inputs) = Fill(observation_mat(inputs), length(inputs))
+observation_noises(m, inputs::Inputs) = Fill(observation_noise(m, inputs), length(inputs))
+observation_mat(m, inputs::Inputs) = Identity()
+observation_mats(m, inputs::Inputs) = Fill(observation_mat(m, inputs), length(inputs))
 
 
 ################################################################################
 ## Delegations
 
 # Rename to `Inputs(model; kwargs...)`?
-mk_inputs(model::Model; kwargs...) = input_type(typeof(model))(model; kwargs...)
+Inputs(model::Model, N::Int; kwargs...) = input_type(typeof(model))(N; kwargs...)
 
-kalman_filter(model::Model, initial_state; kwargs...) =
-    kalman_filter(mk_inputs(model; kwargs...), initial_state)
-kalman_filter(inputs::Inputs, initial_state) = 
-    kalman_filter(initial_state, observations(inputs),
-                  observation_noises(inputs);
-                  transition_mats=transition_mats(inputs),
-                  transition_noises=transition_noises(inputs),
-                  observation_mats=observation_mats(inputs))
+kalman_filter(m::Model, inputs::Inputs, observations::AbstractVector, initial_state) = 
+    kalman_filter(initial_state, observations,
+                  observation_noises(m, inputs);
+                  transition_mats=transition_mats(m, inputs),
+                  transition_noises=transition_noises(m, inputs),
+                  observation_mats=observation_mats(m, inputs))
 
-kalman_smoother(inputs::Inputs, filtered_states::AbstractArray{<:Gaussian}) = 
+kalman_smoother(m::Model, inputs::Inputs, filtered_states::AbstractVector{<:Gaussian})=
     kalman_smoother(filtered_states;
-                    transition_mats=transition_mats(inputs),
-                    transition_noises=transition_noises(inputs))
-kalman_smoother(inputs::Inputs, initial_state::Gaussian) =
-    kalman_smoother(inputs, kalman_filter(inputs, initial_state)[1])
+                    transition_mats=transition_mats(m, inputs),
+                    transition_noises=transition_noises(m, inputs))
+kalman_smoother(m::Model, inputs::Inputs, observations::AbstractVector,
+                initial_state::Gaussian) =
+    kalman_smoother(inputs, kalman_filter(m, inputs, observations, initial_state)[1])
 
-kalman_sample(inputs::Inputs, rng::AbstractRNG, initial_state) =
-    kalman_sample(rng, initial_state, observation_noises(inputs);
-                  transition_mats=transition_mats(inputs),
-                  transition_noises=transition_noises(inputs),
-                  observation_mats=observation_mats(inputs))
+kalman_sample(m::Model, inputs::Inputs, rng::AbstractRNG, initial_state) =
+    kalman_sample(rng, initial_state, observation_noises(m, inputs);
+                  transition_mats=transition_mats(m, inputs),
+                  transition_noises=transition_noises(m, inputs),
+                  observation_mats=observation_mats(m, inputs))
 
 
 ################################################################################
